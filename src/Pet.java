28b08d7ab4dd632d753034997080e9ff82ac97c4
Ho Thang Trinh <hothangtrinh200902@gmail.com>
2022-11-02 14:23:18 +0700

    temporary

--- src/Pet.java
+++ src/Pet.java
@@ -1,17 +1,46 @@
 import java.io.Serializable;
 
 public class Pet implements Serializable {
-    String name;
-    String type;
-    int age;
+    private String name;
+    private String type;
+    private int age;
+
+    public Pet(String name, String type, int age) {
+        this.name = name;
+        this.type = type;
+        this.age = age;
+    }
 
     public static void makeNoise() {
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     @Override
     public String toString() {
         return "Pet [type=" + type + ", name=" + name + ", age=" + age + "]";
     }
-
 }
--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     //c.	Attribute IOService iOService;
-
+    public static void main(String[] args) {
+        //d.	A main method, you should:
+        //a.	Create a list of pets and call method savePet() from IOService  to save this list to file pets.txt
+        //b.	Call method getPets to get list saved pet, loop in this list and call makeNoise() method.
+        Pet p = new Pet("Mozart", "cat", 5);
+        Pet p1 = new Pet("Mozart1", "cat1", 51);
+        Pet p2 = new Pet("Mozart", "cat2", 52);
+        List<Pet> list = new ArrayList<>();
+        list.add(p);
+        list.add(p1);
+        list.add(p2);
+        try {
+            FileWriter fileWriter = new FileWriter("src/Pet.txt",true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            for (Pet o : list) {
+                bufferedWriter.write(o.toString());
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+            fileWriter.close();
+        } catch (Exception e) {}
+    }
 }
